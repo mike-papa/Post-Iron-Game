fadd021977dd644b7c0c2037be67bca67e757fd8
mike <mike>
2023-11-09 20:15:33 +0100

    Add initial-view

--- src/main/java/com/example/postirongame/Main.java
+++ src/main/java/com/example/postirongame/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("game-character-class-select.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("initial-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 400);
         stage.setTitle("Post-Iron Game!");
         stage.setScene(scene);
